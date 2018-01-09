a0d313dd58da2ad60973603c437c58fc74865b1d
JamesieBondco <jspencer@playon.co>
2018-01-09 00:41:47 +0000

    Asset packs. Modify Velocity calcuation & Acceleration Calcuation

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -9,8 +9,8 @@ public class MouseLook : MonoBehaviour {
 	public float sensitivity = 1.0f;
 	public float smoothing =  2.0f;
 
-	public Text mouseSmoothnessText;
-	public Text mouseSpeedText;
+	public Text mouseAccelerationText;
+	public Text mouseVelocityText;
 
 	Vector3 initialPosition;
 	Vector3 currentPosition;
@@ -18,8 +18,27 @@ public class MouseLook : MonoBehaviour {
 
 	GameObject character;
 
+	private float timeInterval = 0f;
+
 	private float mouseSmoothness = 0f;
-	private float mouseSpeed = 0f;
+	private float mouseDistance = 0f;
+	private float mouseVelocity = 0f;
+	private float mouseAcceleration = 0f;
+
+	private float initialSpeed = 0f;
+
+	private float mouseXMovement = 0f;
+	private float mouseYMovement = 0f;
+
+	private int updateCount = 0;
+	private float timeCount;
+
+
+	private bool resetRequired = false;
+
+	void OnGUI() {
+		GUI.Box (new Rect (1000, 0, 100, 50), (1 / Time.deltaTime).ToString ());
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -35,26 +54,63 @@ public class MouseLook : MonoBehaviour {
 
 		md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
 		smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f/ smoothing);
-		smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f/ smoothing);
+//		smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f/ smoothing);
 		mouseLook += smoothV;
 		mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
 
 		currentPosition = Camera.main.WorldToScreenPoint(Input.mousePosition);
+		print ("WorldToScreenPoint = " + Camera.main.ScreenToWorldPoint(Input.mousePosition) + "WorldToViewportPoint = " + Camera.main.WorldToViewportPoint(Input.mousePosition));
+//		currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		if (previousPosition == Vector3.zero) {
 			previousPosition = initialPosition;
 		}
-		mouseSpeed = Mathf.Abs(currentPosition.x - previousPosition.x) + Mathf.Abs(currentPosition.y - previousPosition.y);
-		previousPosition = Camera.main.WorldToScreenPoint(Input.mousePosition);
 
-		updateTextValues();
+		if (updateCount < 5) {
+//			print ("Update Time: " + Time.deltaTime);
+			timeCount += Time.deltaTime;
+			updateCount++;
+		}
+
+		if (timeInterval < 0.2f) {
+			timeInterval += Time.deltaTime;
+			mouseXMovement += (Mathf.Abs (currentPosition.x) - Mathf.Abs (previousPosition.x)) / 100f;
+			mouseYMovement += (Mathf.Abs (currentPosition.y) - Mathf.Abs (previousPosition.y)) / 100f;
+		} else {
+			mouseXMovement += (Mathf.Abs (currentPosition.x) - Mathf.Abs (previousPosition.x)) / 100f;
+			mouseYMovement += (Mathf.Abs (currentPosition.y) - Mathf.Abs (previousPosition.y)) / 100f;
+			timeInterval += Time.deltaTime;
+			mouseDistance = mouseXMovement + mouseYMovement;
+			mouseVelocity = mouseDistance / timeInterval;
+			mouseAcceleration = mouseVelocity - initialSpeed / timeInterval;
+			print ("Mouse Acceleration: " + mouseAcceleration);
+			initialSpeed = mouseVelocity;
+			timeInterval = 0f;
+			mouseXMovement = 0;
+			mouseYMovement = 0;
+		}
+		 
+		previousPosition = Camera.main.WorldToScreenPoint(Input.mousePosition);
+//		previousPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+		updateTextValues ();
+
+		if (updateCount > 5) {
+			resetCalculatedValues ();
+		}
 
 		transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
 		character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
 	}
 
 	void updateTextValues () {
-		mouseSmoothnessText.text = "Smoothness: " + mouseSmoothness;
-		mouseSpeedText.text = "Speed: " + mouseSpeed;
+		mouseAccelerationText.text = "Acceleration: " + mouseAcceleration;
+		mouseVelocityText.text = "Speed: " + mouseVelocity;
 	}
 
+	void resetCalculatedValues () {
+		print ("Ten frames update: " + timeCount);
+		timeCount = Time.deltaTime;
+
+		updateCount = 0;
+	}
 }
